d4fd8416b08db7746a6ca3dab96149b1a91016ba
Melik Karapetyan <melik@krisp.ai>
2024-10-24 02:43:05 +0400

    FIX: workflow

--- main/github.runner.tf
+++ main/github.runner.tf
@@ -48,3 +48,58 @@ resource "aws_key_pair" "github_runner_key" {
   key_name   = "${local.prefix}-github-runner-key"
   public_key = local.github_public_key
 }
+
+# IAM Instance Profile for the runner
+resource "aws_iam_instance_profile" "runner_profile" {
+  name = "runner-ec2-profile"
+  role = aws_iam_role.runner_role.name
+}
+
+# IAM Policy for the runner
+resource "aws_iam_policy" "runner_policy" {
+  name        = "runner-policy"
+  path        = "/"
+  description = "Policy for runner EC2 instance"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "*"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "runner_role" {
+  name = local.ec2_runner_iam_role
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${local.github_org}/${local.github_repo_name}:*"
+          }
+        }
+      },
+    ]
+  })
+}
+
+# Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "runner_policy_attachment" {
+  role       = aws_iam_role.runner_role.name
+  policy_arn = aws_iam_policy.runner_policy.arn
+}
