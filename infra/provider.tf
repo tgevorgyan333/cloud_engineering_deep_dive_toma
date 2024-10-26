df134ad19475d49bdbb346a014a42b9347c5ca8c
Melik Karapetyan <melik@krisp.ai>
2024-10-26 19:28:41 +0400

    fix formatting

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
   default_tags {
     tags = {
-      Env = terraform.workspace
-      ManagedBy   = "terraform"
-      Project     = local.project_name
+      Env       = terraform.workspace
+      ManagedBy = "terraform"
+      Project   = local.project_name
     }
   }
 }
