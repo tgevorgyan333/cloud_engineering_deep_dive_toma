a7e8e19f9102e4643f181a36ae7571d889a2a5d9
Melik Karapetyan <melik@krisp.ai>
2024-10-26 19:38:10 +0400

    move project name to secrets manager

--- infra/locals.tf
+++ infra/locals.tf
@@ -8,11 +8,11 @@ data "aws_region" "current" {}
 
 locals {
   az_suffix    = ["a", "b", "c", "d", "e", "f"]
-  project_name = var.project_name
 
   # Core Component Configurations.
   core_name_prefix = "${terraform.workspace}.core"
   core_az_count    = module.network_secret_ro.secret_map["core_az_count"]
   core_vpc_cidr    = module.network_secret_ro.secret_map["core_vpc_cidr"]
+  project_name     = module.network_secret_ro.secret_map["project_name"]
   core_region      = data.aws_region.current.name
 }
